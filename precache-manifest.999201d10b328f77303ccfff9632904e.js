4b60df8f48a0b2761430cd35671713274ef30795
Zidan <midoz393@gmail.com>
2020-09-23 11:33:40 -0400

    Updates

---
+++ precache-manifest.999201d10b328f77303ccfff9632904e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2943df596b74ddc53f6977bc8d6ec1f",
+    "url": "/little-rocket/index.html"
+  },
+  {
+    "revision": "13e7f45caa72a78485c5",
+    "url": "/little-rocket/static/css/main.ea4f8212.chunk.css"
+  },
+  {
+    "revision": "301d56d1f3511aa00001",
+    "url": "/little-rocket/static/js/2.01cf3053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/little-rocket/static/js/2.01cf3053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e7f45caa72a78485c5",
+    "url": "/little-rocket/static/js/main.6241f43d.chunk.js"
+  },
+  {
+    "revision": "cc21cb72a822951fb20b",
+    "url": "/little-rocket/static/js/runtime-main.d8211c98.js"
+  }
+]);
